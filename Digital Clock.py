d6d9f7fd71070493b713d0da83bee9566f6fe06e
d3v4nxh <devanshsingh9457@gmail.com>
2023-10-29 01:27:08 +0530

    test 2

--- Digital Clock.py
+++ Digital Clock.py
@@ -1,16 +1,20 @@
+#import libraries and there elements 
 from tkinter import *
 from tkinter.ttk import *
 from time import strftime
+#create the main window of our code 
 root = Tk()
 root.title("clock")
+#define function in which we have to declare the time format and other things
 def time():
     string = strftime("%H:%M:%S%p")
     label.config(text=string)
     label.after(1000,time)
 
-
+#define  appearance of clock
 label = Label(root, font =("ds-digital",80),background="black",foreground="magenta")
 label.pack(anchor='center')
+# callout the function 
 time()
-
+#use this to tell tkinter to run event loop (refresh rate )
 mainloop()
